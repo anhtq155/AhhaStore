8f3ab2d106044b8841f04fc55c30f5e4a73c7ead
Tran Quoc Anh <100049>
2024-03-13 15:43:45 +0700

    cm đang lỗi

---
+++ AHclient/src/features/catalog/Catalog.tsx
@@ -0,0 +1,10 @@
+
+export default function Catalog(props: any) {
+    return {
+        <ul>
+            {props.products.map((product: any) => (
+                <li key = {product.id}> {product.name} - {product.price} </li>
+            ))}
+        </ul>
+    }
+}
